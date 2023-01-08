860de1c26b07706f3471d6aadf83654f6b3425ea
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-01-08 17:58:10 +0100

    GregorianCalendarToJulianDate

---
+++ Source/SOFA.NET.Test/Calendars/CalendarsModuleTest.cs
@@ -0,0 +1,21 @@
+﻿namespace SOFA.NET.Test;
+
+[TestFixture]
+internal class CalendarsModuleTest
+{
+
+    #region Tests
+
+    [Test]
+    public void GregorianCalendarToJulianDate_Test()
+    {
+        DateTime dateTime = new(2003, 6, 1);
+
+        JulianDate result = CalendarsModule.GregorianCalendarToJulianDate(dateTime);
+
+        Assert.That(result.Date, Is.EqualTo(2452791.5));
+    }
+
+    #endregion
+
+}
---
+++ Source/SOFA.NET/Calendars/CalendarsModule.cs
@@ -0,0 +1,30 @@
+﻿namespace SOFA.NET;
+
+public static class CalendarsModule
+{
+    /// <summary>
+    /// Gregorian Calendar to Julian Date.
+    /// SOFA name: iauCal2jd
+    /// </summary>
+    /// <param name="dateTime">Date time as gregorian calendar</param>
+    /// <returns>Returns Modified Julian Date for 0 hours</returns>
+    public static JulianDate GregorianCalendarToJulianDate(DateTime dateTime)
+    {
+        int my;
+        long iypmy;
+        int iy = dateTime.Year,
+            im = dateTime.Month,
+            id = dateTime.Day;
+
+        // Return result
+        my = (im - 14) / 12;
+        iypmy = iy + my;
+        double result = ((1461L * (iypmy + 4800L)) / 4L
+                + (367L * (im - 2 - 12 * my)) / 12L
+                - (3L * ((iypmy + 4900L) / 100L)) / 4L
+                + id - 2432076L);
+
+        return new(result + Constants.DJM0);
+    }
+
+}
