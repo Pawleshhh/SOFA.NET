1e9b7006ed43a0cd0a5c0c19c162f896260e3a89
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2023-01-14 17:25:50 +0100

    Refactor Coordinate systems

--- Source/SOFA.NET/Coordinates/Systems/CoordinateDefinitions.cs
+++ Source/SOFA.NET/Coordinates/Systems/CoordinateDefinitions.cs
@@ -1,19 +1,4 @@
 ﻿namespace SOFA.NET;
 
-public record GeographicCoordinates : CoordinateSystem<double>, ICoordinateSystem2D<double>
-{
-
-    public GeographicCoordinates(double latitude, double longitude)
-        : base(latitude, longitude)
-    {
-        Latitude = latitude;
-        Longitude = longitude;
-    }
-
-    double ICoordinateSystem2D<double>.X { get => Get(0); set => Set(0, value); }
-    double ICoordinateSystem2D<double>.Y { get => Get(1); set => Set(1, value); }
-
-    public double Latitude { get => Get(0); set => Set(0, value); }
-    public double Longitude { get => Get(1); set => Set(1, value); }
-
-}
+public record GeographicCoordinates(double Latitude, double Longitude)
+    : CoordinateSystemBase<double>(Latitude, Longitude), ICoordinateSystem2D<double>;
---
+++ Source/SOFA.NET/Coordinates/Systems/ICoordinateSystem2D.cs
@@ -0,0 +1,20 @@
+﻿namespace SOFA.NET;
+
+public interface ICoordinateSystem2D<T> : ICoordinateSystem<T>
+    where T : struct
+{
+    T X { get => Get(0); }
+    T Y { get => Get(1); }
+
+    public void Deconstruct(out T x, out T y)
+    {
+        x = X;
+        y = Y;
+    }
+
+    public static ICoordinateSystem2D<T> Create(T x, T y)
+        => new CoordinateSystem2D<T>(x, y);
+}
+
+internal record CoordinateSystem2D<T>(T X, T Y) 
+    : CoordinateSystemBase<T>(X, Y), ICoordinateSystem2D<T> where T : struct;
