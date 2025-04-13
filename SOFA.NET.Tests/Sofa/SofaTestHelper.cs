c9cd2460bc3f523245ced7ede6bed4388db64ba3
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2025-04-13 10:57:12 +0200

    Port SofaCalendars functions

---
+++ SOFA.NET.Tests/Sofa/SofaTestHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SOFA.NET.Tests;
+
+internal static class SofaTestHelper
+{
+
+    public static void AssertLastError(bool occured = false)
+    {
+        var err = Marshal.GetLastWin32Error();
+        if (occured)
+        {
+            Assert.That(err, Is.Not.EqualTo(0));
+        }
+        else
+        {
+            Assert.That(err, Is.EqualTo(0));
+        }
+    }
+
+}
---
+++ SOFA.NET/Sofa/SofaDllHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace SOFA.NET;
+
+static class SofaDllHelper
+{
+
+    public const string DllPath = "./sofa_c.dll";
+
+}
