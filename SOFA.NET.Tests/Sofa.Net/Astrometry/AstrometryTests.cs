d019544cd003ad990248ea34e8309320defc58a5
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2025-04-22 17:45:58 +0200

    ApplyAberration_Test

---
+++ SOFA.NET.Tests/Sofa.Net/Astrometry/AstrometryTests.cs
@@ -0,0 +1,31 @@
+﻿namespace SOFA.NET.Tests;
+
+[TestFixture]
+internal class AstrometryTests
+{
+
+    [Test]
+    public void ApplyAberration_Test()
+    {
+        var pnat = new Vector3<double>(
+            -0.76321968546737951,
+            -0.60869453983060384,
+            -0.21676408580639883);
+        var v = new Vector3<double>(
+            2.1044018893653786e-5,
+            -8.9108923304429319e-5,
+            -3.8633714797716569e-5);
+        var s = 0.99980921395708788;
+        var bm1 = 0.99999999506209258;
+
+        var result = Astrometry.ApplyAberration(pnat, v, s, bm1);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result[0], Is.EqualTo(-0.7631631094219556269).Within(1e-12));
+            Assert.That(result[1], Is.EqualTo(-0.6087553082505590832).Within(1e-12));
+            Assert.That(result[2], Is.EqualTo(-0.2167926269368471279).Within(1e-12));
+        });
+    }
+
+}
---
+++ SOFA.NET/Sofa.Net/Astrometry/Astrometry.cs
@@ -0,0 +1,32 @@
+﻿namespace SOFA.NET;
+
+public class Astrometry
+{
+
+    /// <summary>
+    /// Apply aberration to transform natural direction into proper
+    /// direction.
+    /// </summary>
+    /// <param name="naturalDirection">Natural direction to the source.</param>
+    /// <param name="observerBarycentricVelocity">Observer barycentric velocity in units of c.</param>
+    /// <param name="sunObserverDistance">Distance between the Sun and the observer (au).</param>
+    /// <param name="lorenzFactor">sqrt(1-|v|^2): reciprocal of Lorenz factor.</param>
+    /// <returns>Proper direction to source.</returns>
+    public static Vector3<double> ApplyAberration(
+        Vector3<double> naturalDirection,
+        Vector3<double> observerBarycentricVelocity,
+        double sunObserverDistance,
+        double lorenzFactor)
+    {
+        var result = new double[3];
+        SofaAstrometry.Ab(
+            naturalDirection.AsArray(),
+            observerBarycentricVelocity.AsArray(),
+            sunObserverDistance,
+            lorenzFactor,
+            result);
+
+        return new(result[0], result[1], result[2]);
+    }
+
+}
